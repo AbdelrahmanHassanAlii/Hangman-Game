c10e7fe64617531e40604edcf1e9cdf8021d8a0c
venom <abdo30201080101694@gmail.com>
2023-09-02 10:26:37 +0300

    elzero end

--- index.js
+++ index.js
@@ -2,22 +2,46 @@ let letters = "abcdefghijklmnopqrstuvwxyz";
 
 let lettersArray = Array.from(letters);
 
-let lettersContainer = document.querySelector('.letters');
+let lettersContainer = document.querySelector(".letters");
 
-lettersArray.forEach(letter => {
-    let span = document.createElement('span');
-    span.textContent = letter;
-    span.className = 'letter-box';
-    lettersContainer.appendChild(span);
+lettersArray.forEach((letter) => {
+  let span = document.createElement("span");
+  span.textContent = letter;
+  span.className = "letter-box";
+  lettersContainer.appendChild(span);
 });
 
 // Object Of Words + Categories
 const words = {
-    programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python"],
-    movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up"],
-    people: ["Albert Einstein", "Hitchcock", "Alexander", "Cleopatra", "Mahatma Ghandi"],
-    countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"]
-}
+  programming: [
+    "php",
+    "javascript",
+    "go",
+    "scala",
+    "fortran",
+    "r",
+    "mysql",
+    "python",
+  ],
+  movies: [
+    "Prestige",
+    "Inception",
+    "Parasite",
+    "Interstellar",
+    "Whiplash",
+    "Memento",
+    "Coco",
+    "Up",
+  ],
+  people: [
+    "Albert Einstein",
+    "Hitchcock",
+    "Alexander",
+    "Cleopatra",
+    "Mahatma Ghandi",
+  ],
+  countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"],
+};
 //get the all keys of myObject
 let allKeys = Object.keys(words);
 //get a random number depends on the keys lenght
@@ -31,7 +55,7 @@ let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
 //get a randon value
 let randomValueValue = randomPropValue[randomValueNumber];
 
-document.querySelector('.game-info .category span').innerHTML = randomPropName;
+document.querySelector(".game-info .category span").innerHTML = randomPropName;
 
 console.log(allKeys);
 console.log(randomPropNumber);
@@ -40,29 +64,77 @@ console.log(randomPropValue);
 console.log(randomValueNumber);
 console.log(randomValueValue);
 
-let lettersGuessContainer =  document.querySelector('.letters-guess');
+let lettersGuessContainer = document.querySelector(".letters-guess");
 
-let lettersAndSpace = Array.from(randomValueValue)
+let lettersAndSpace = Array.from(randomValueValue);
 console.log(lettersAndSpace);
 
-lettersAndSpace.forEach(letter => {
-    let emptySpan = document.createElement('span');
-    if(letter === ' '){
-        emptySpan.className = 'with-space';
-    }
-    lettersGuessContainer.appendChild(emptySpan);
+lettersAndSpace.forEach((letter) => {
+  let emptySpan = document.createElement("span");
+  if (letter === " ") {
+    emptySpan.className = "with-space";
+  }
+  lettersGuessContainer.appendChild(emptySpan);
 });
 
-document.addEventListener('click', (e) => {
-    if(e.target.className === 'letter-box'){
-        e.target.classList.add('clicked');
-        let theClickedLetter = e.target.innerHTML.toLowerCase();
-        console.log(theClickedLetter);
-        lettersAndSpace.forEach((wordLetter, index) => {
-            if(wordLetter === theClickedLetter){
-                console.log(`found at index ${index}`);
-            }
+let theChosenWord = Array.from(randomValueValue.toLowerCase());
+console.log(lettersAndSpace);
+console.log(theChosenWord);
+
+let guessSpans = document.querySelectorAll(".letters-guess span");
+
+let wrongAttemps = 0;
+
+let theDraw = document.querySelector(".hangman-draw");
+
+document.addEventListener("click", (e) => {
+  let theStatus = false;
+  if (e.target.className === "letter-box") {
+    e.target.classList.add("clicked");
+    let theClickedLetter = e.target.innerHTML.toLowerCase();
+    console.log(theClickedLetter);
+    theChosenWord.forEach((wordLetter, wordIndex) => {
+      if (wordLetter === theClickedLetter) {
+        console.log(`found at index ${wordIndex}`);
+        theStatus = true;
+        guessSpans.forEach((span, spanIndex) => {
+          if (wordIndex === spanIndex) {
+            span.innerHTML = theClickedLetter;
+          }
         });
+      }
+    });
+    console.log(theStatus);
+    if (theStatus === false) {
+      wrongAttemps++;
+      theDraw.classList.add(`wrong-${wrongAttemps}`);
+      document.getElementById("fail").play();
+
+      if (wrongAttemps === 8) {
+        endGame();
+        lettersContainer.classList.add("finished");
+      }
+    } else {
+      document.getElementById("success").play();
     }
+  }
 });
 
+function endGame() {
+  // Create Popup Div
+  let div = document.createElement("div");
+
+  // Create Text
+  let divText = document.createTextNode(
+    `Game Over, The Word Is ${randomValueValue}`
+  );
+
+  // Append Text To Div
+  div.appendChild(divText);
+
+  // Add Class On Div
+  div.className = "popup";
+
+  // Append To The Body
+  document.body.appendChild(div);
+}
